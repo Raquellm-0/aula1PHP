0f0a0c09ecccf1f091cb21bf1e7560566c783c7e
Raquel Leirião Marques <raquelmarques2503@gmail.com>
2024-10-30 16:54:13 -0400

    tesk001

---
+++ exemploVariavel.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "Raquel";
+    $sobrenome = "Leirião";
+    $idade = "17";
+    $profissao = "estudante";
+
+    echo "oi, $profissao $nome $sobrenome. <br> ";
+    echo "Você possui atualmente $idade anos.";
+?>
